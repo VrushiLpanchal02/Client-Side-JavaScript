e12a7bc142a9d40c3b02e6a3120d06feb09765b5
Vrushil Panchal <vrushil2446@gmail.com>
2026-06-06 11:54:49 -0400

    Add starter variables for silly character

---
+++ sillyCharacter.js
@@ -0,0 +1,7 @@
+// Declare and initialize variables
+let characterName = "Captain Pickle";
+let age = Math.floor(Math.random() * 60) + 10;
+let isSuperhero = Math.random() > 0.5;
+let specialPowers = ["laser eyes", "super speed", "teleportation", "mind reading", "flying"];
+let favoriteFood = "pizza";
+
